d8384d1b97448556159fcac7eb845779c1ce0208
Jason Kim <j.kim@syncapse.com>
2011-11-25 23:36:19 -0500

    again added layout

---
+++ application/views/layouts/default.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+
+<!-- Envysea Header (normal pages) -->
+
+<head>
+    <!-- meta tags -->
+    <meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>
+    <meta name="description"
+          content="Envysea authentication offers rapid application development for membership, authentication, and login sites."/>
+    <meta name="keywords" content="Authentication, Membership, Login, Codeigniter 2.0.3"/>
+    <meta name="author" content="Nicholas Cerminara | http://envysea.com"/>
+
+    <!-- favicon -->
+    <link href="http://lostandcloud.com/assets/images/favicon.png" rel="shortcut icon" type="image/png"/>
+    <!-- css -->
+    <link href="/css/reset.css" rel="stylesheet" type="text/css"/>
+    <!-- Eric Meyer CSS reset v2.0 (HTML5 and CSS3 support) -->
+    <link href="/css/base.css" rel="stylesheet" type="text/css"/>
+    <!-- Shared CSS Elements -->
+    <link href="/css/style.css" rel="stylesheet" type="text/css"/>
+    <!-- Unique to non-admin and non-secure pages only -->
+
+    <!-- javascript -->
+    <!--[if IE]>
+    <script src="/js/html5.js"></script><![endif]-->
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <!-- jQuery v1.7 -->
+    <script type="text/javascript" src="/js/scripts.js"></script>
+    <!-- custom jQuery scripts/plugins -->
+
+    <title>Envysea Codeigniter Authentication</title>
+
+</head>
+<body>
+<div id="container">
+    <div id="header">
+        <!-- delete this -->
+        <div id="logo">
+            <a href="http://envysea.com/showcase/open/">Home</a></div>
+        <!-- /end delete this -->
+        <ul id="nav">
+            <li><a href="http://lostandcloud.com/" title="Home is where you make it.">Home</a></li>
+            <li><a href="http://lostandcloud.com/envysea/about">About</a></li>
+            <li><a href="http://lostandcloud.com/members">Members</a></li>
+            <li><a href="http://lostandcloud.com/envysea/contact">Contact</a></li>
+        </ul>
+    </div>
+    <div id="content">
+
+        <h1 class="page_header">Open Source Authentication for Codeigniter</h1>
+
+        <h2>Codeigniter:</h2>Version 2.0.3
+        <br><br>
+
+        <h2>Last Updated:</h2>November 06, 2011
+        <br><br>
+
+        <h2>Authored by:</h2>Nicholas Cerminara
+        <br><br>
+        <span style="color: #63B8FF">Functionality is limited in demo to prevent too many database changes.<span>
+        <br>
+    </div>
+    <!-- #container -->
+</body>
+</html>
