cee689741d3b61ec1fb6c2ac928015a1ad4e53ea
Jason Kim <j.kim@syncapse.com>
2011-12-27 02:06:43 -0500

    update UI version

--- application/views/members/free_print.php
+++ application/views/members/free_print.php
@@ -1,112 +1,129 @@
 <!-- include css files -->
-<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/base/jquery-ui.css" id="theme">
-<link rel="stylesheet" href="http://blueimp.github.com/jQuery-Image-Gallery/jquery.image-gallery.css">
+<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+<link rel="stylesheet" href="http://blueimp.github.com/Bootstrap-Image-Gallery/bootstrap-image-gallery.min.css">
 <link rel="stylesheet" href="/css/uploader/jquery.fileupload-ui.css">
+<style type="text/css">
+.page-header {
+    background-color: #f5f5f5;
+    padding: 80px 20px 10px;
+    margin: 0 -20px 20px;
+    border: 1px solid #DDD;
+    -webkit-border-radius: 0 0 6px 6px;
+       -moz-border-radius: 0 0 6px 6px;
+            border-radius: 0 0 6px 6px;
+}
+</style>
 
 <!-- include javascript files -->
-<script type="text/javascript" src="/js/jquery-ui-1.8.16.custom.min.js"></script>
-<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js"></script>
-<script type="text/javascript" src="http://blueimp.github.com/jQuery-Image-Gallery/jquery.image-gallery.js"></script>
-<script type="text/javascript" src="/js/uploader/jquery.iframe-transport.js"></script>
-<script type="text/javascript" src="/js/uploader/jquery.xdr-transport.js"></script>
-<script type="text/javascript" src="/js/uploader/jquery.fileupload.js"></script>
-<script type="text/javascript" src="/js/uploader/jquery.fileupload-ui.js"></script>
-<script type="text/javascript" src="/js/uploader/application.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="/js/uploader/vendor/jquery.ui.widget.js"></script>
+<script src="http://blueimp.github.com/JavaScript-Templates/tmpl.min.js"></script>
+<script src="http://blueimp.github.com/JavaScript-Load-Image/load-image.min.js"></script>
+<!-- Bootstrap Modal and Image Gallery are not required, but included for the demo -->
+<script src="http://twitter.github.com/bootstrap/1.4.0/bootstrap-modal.min.js"></script>
+<script src="http://blueimp.github.com/Bootstrap-Image-Gallery/bootstrap-image-gallery.min.js"></script>
+<script src="/js/uploader/jquery.iframe-transport.js"></script>
+<script src="/js/uploader/jquery.fileupload.js"></script>
+<script src="/js/uploader/jquery.fileupload-ui.js"></script>
+<script src="/js/uploader/application.js"></script>
+<!-- The XDomainRequest Transport is included for cross-domain file deletion for IE >= 8 -->
+<script src="/js/uploader/jquery.xdr-transport.js"></script>
+<!-- postMessage Transport support can be added with the following plugin -->
+<!--script src="jquery.postmessage-transport.js"></script-->
 
 <?php echo heading('Print your documents now', 1, 'class="page_header"'); ?>
 
 <div id="home_view">
 
-    <div id="fileupload">
-        <form action="/members/upload" method="POST" enctype="multipart/form-data">
-            <div class="fileupload-buttonbar">
-                <label class="fileinput-button">
+    <form id="fileupload" action="/members/upload" method="POST" enctype="multipart/form-data">
+        <div class="row">
+            <div class="span16 fileupload-buttonbar">
+                <div class="progressbar fileupload-progressbar"><div style="width:0%;"></div></div>
+                <span class="btn success fileinput-button">
                     <span>Add files...</span>
                     <input type="file" name="files[]" multiple>
-                </label>
-                <button type="submit" class="start">Start upload</button>
-                <button type="reset" class="cancel">Cancel upload</button>
-                <button type="button" class="delete">Delete selected</button>
+                </span>
+                <button type="submit" class="btn primary start">Start upload</button>
+                <button type="reset" class="btn info cancel">Cancel upload</button>
+                <button type="button" class="btn danger delete">Delete selected</button>
                 <input type="checkbox" class="toggle">
             </div>
-        </form>
-        <div class="fileupload-content">
-            <table class="files"></table>
-            <div class="fileupload-progressbar"></div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="span16">
+                <table class="zebra-striped"><tbody class="files"></tbody></table>
+            </div>
+        </div>
+    </form>
+
+    <!-- gallery-loader is the loading animation container -->
+    <div id="gallery-loader"></div>
+    <!-- gallery-modal is the modal dialog used for the image gallery -->
+    <div id="gallery-modal" class="modal hide fade">
+        <div class="modal-header">
+            <a href="#" class="close">&times;</a>
+            <h3 class="title"></h3>
+        </div>
+        <div class="modal-body"></div>
+        <div class="modal-footer">
+            <a class="btn primary next">Next</a>
+            <a class="btn info prev">Previous</a>
+            <a class="btn success download" target="_blank">Download</a>
         </div>
     </div>
-    <div id="notes">
-        <h3>Demo Notes</h3>
-        <ul>
-            <li>The maximum file size for uploads in this demo is <strong>5 MB</strong> (default file size is unlimited).</li>
-            <li>Only image files (<strong>JPG, GIF, PNG</strong>) are allowed in this demo (by default there is no file type restriction).</li>
-            <li>Uploaded files will be deleted automatically after <strong>5 minutes</strong> (demo setting).</li>
-            <li>You can <strong>drag &amp; drop</strong> files from your desktop on this webpage with Google Chrome, Mozilla Firefox and Apple Safari.</li>
-            <li>Please refer to the project <a href="https://github.com/blueimp/jQuery-File-Upload">homepage</a> and <a href="https://github.com/blueimp/jQuery-File-Upload/wiki">documentation</a> for more information.</li>
-        </ul>
-    </div>
-    
-    <script id="template-upload" type="text/x-jquery-tmpl">
-        <tr class="template-upload{{if error}} ui-state-error{{/if}}">
-            <td class="preview"></td>
-            <td class="name">{{if name}}${name}{{else}}Untitled{{/if}}</td>
-            <td class="size">${sizef}</td>
-            {{if error}}
-                <td class="error" colspan="2">Error:
-                    {{if error === 'maxFileSize'}}File is too big
-                    {{else error === 'minFileSize'}}File is too small
-                    {{else error === 'acceptFileTypes'}}Filetype not allowed
-                    {{else error === 'maxNumberOfFiles'}}Max number of files exceeded
-                    {{else}}${error}
-                    {{/if}}
-                </td>
-            {{else}}
-                <td class="progress"><div></div></td>
-                <td class="start"><button>Start</button></td>
-            {{/if}}
-            <td class="cancel"><button>Cancel</button></td>
-        </tr>
+    <script>
+    var fileUploadErrors = {
+        maxFileSize: 'File is too big',
+        minFileSize: 'File is too small',
+        acceptFileTypes: 'Filetype not allowed',
+        maxNumberOfFiles: 'Max number of files exceeded',
+        uploadedBytes: 'Uploaded bytes exceed file size',
+        emptyResult: 'Empty file upload result'
+    };
     </script>
-    <script id="template-download" type="text/x-jquery-tmpl">
-        <tr class="template-download{{if error}} ui-state-error{{/if}}">
-            {{if error}}
-                <td></td>
-                <td class="name">${name}</td>
-                <td class="size">${sizef}</td>
-                <td class="error" colspan="2">Error:
-                    {{if error === 1}}File exceeds upload_max_filesize (php.ini directive)
-                    {{else error === 2}}File exceeds MAX_FILE_SIZE (HTML form directive)
-                    {{else error === 3}}File was only partially uploaded
-                    {{else error === 4}}No File was uploaded
-                    {{else error === 5}}Missing a temporary folder
-                    {{else error === 6}}Failed to write file to disk
-                    {{else error === 7}}File upload stopped by extension
-                    {{else error === 'maxFileSize'}}File is too big
-                    {{else error === 'minFileSize'}}File is too small
-                    {{else error === 'acceptFileTypes'}}Filetype not allowed
-                    {{else error === 'maxNumberOfFiles'}}Max number of files exceeded
-                    {{else error === 'uploadedBytes'}}Uploaded bytes exceed file size
-                    {{else error === 'emptyResult'}}Empty file upload result
-                    {{else}}${error}
-                    {{/if}}
-                </td>
-            {{else}}
-                <td class="preview">
-                    {{if thumbnail_url}}
-                        <a href="${url}" title="${name}" rel="gallery"><img src="${thumbnail_url}"></a>
-                    {{/if}}
-                </td>
-                <td class="name">
-                    <a href="${url}" title="${name}"{{if thumbnail_url}} rel="gallery"{{/if}}>${name}</a>
-                </td>
-                <td class="size">${sizef}</td>
+    <script id="template-upload" type="text/html">
+    {% for (var i=0, files=o.files, l=files.length, file=files[0]; i<l; file=files[++i]) { %}
+        <tr class="template-upload fade">
+            <td class="preview"><span class="fade"></span></td>
+            <td class="name">{%=file.name%}</td>
+            <td class="size">{%=o.formatFileSize(file.size)%}</td>
+            {% if (file.error) { %}
+                <td class="error" colspan="2"><span class="label important">Error</span> {%=fileUploadErrors[file.error] || file.error%}</td>
+            {% } else if (o.files.valid && !i) { %}
+                <td class="progress"><div class="progressbar"><div style="width:0%;"></div></div></td>
+                <td class="start">{% if (!o.options.autoUpload) { %}<button class="btn primary">Start</button>{% } %}</td>
+            {% } else { %}
                 <td colspan="2"></td>
-            {{/if}}
+            {% } %}
+            <td class="cancel">{% if (!i) { %}<button class="btn info">Cancel</button>{% } %}</td>
+        </tr>
+    {% } %}
+    </script>
+    <script id="template-download" type="text/html">
+    {% for (var i=0, files=o.files, l=files.length, file=files[0]; i<l; file=files[++i]) { %}
+        <tr class="template-download fade">
+            {% if (file.error) { %}
+                <td></td>
+                <td class="name">{%=file.name%}</td>
+                <td class="size">{%=o.formatFileSize(file.size)%}</td>
+                <td class="error" colspan="2"><span class="label important">Error</span> {%=fileUploadErrors[file.error] || file.error%}</td>
+            {% } else { %}
+                <td class="preview">{% if (file.thumbnail_url) { %}
+                    <a href="{%=file.url%}" title="{%=file.name%}" rel="gallery"><img src="{%=file.thumbnail_url%}"></a>
+                {% } %}</td>
+                <td class="name">
+                    <a href="{%=file.url%}" title="{%=file.name%}" rel="{%=file.thumbnail_url&&'gallery'%}">{%=file.name%}</a>
+                </td>
+                <td class="size">{%=o.formatFileSize(file.size)%}</td>
+                <td colspan="2"></td>
+            {% } %}
             <td class="delete">
-                <button data-type="${delete_type}" data-url="${delete_url}">Delete</button>
+                <button class="btn danger" data-type="{%=file.delete_type%}" data-url="{%=file.delete_url%}">Delete</button>
                 <input type="checkbox" name="delete" value="1">
             </td>
         </tr>
+    {% } %}
     </script>
 
 </div>
